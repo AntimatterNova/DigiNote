597667712bf0df13708cf508e0f9f1bfff30abd5
AntimatterNova <dmvsecondary@gmail.com>
2023-06-20 01:21:41 -0500

    Made some minor progress debugging the index.js file

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const api = require('./public/assets/js/index');
+const api = require('./public/assets/js/index.js');
 const uuid = require('./helpers/uuid');
 const app = express();
 const PORT = 3001;
@@ -9,19 +9,28 @@ const PORT = 3001;
 app.use(express.static('public'));
 app.use(express.json());
 
-//loading notes from db.json
+// Loading notes from db.json
 app.get('/api/notes', (req, res) => {
   const note = JSON.parse(fs.readFileSync(path.join(__dirname, "db", "db.json")));
   res.json(note);
 });
-  
+
+// HTML routes
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
-  
+
 app.post('/api/notes', (req, res) => {
   // Log that a POST request was received
-  console.info(`${req.method} request received to add a review`);
+  console.info(`${req.method} request received to add a note`);
 
   // Destructuring assignment for the items in req.body
   const { title, text } = req.body;
@@ -40,9 +49,18 @@ app.post('/api/notes', (req, res) => {
       body: newNote,
     };
 
+    // Read the existing notes from the JSON file
+    const existingNotes = JSON.parse(fs.readFileSync(dbFilePath));
+
+    // Add the new note to the existing notes array
+    existingNotes.push(newNote);
+
+    // Write the updated notes array back to the JSON file
+    fs.writeFileSync(dbFilePath, JSON.stringify(existingNotes));
+
     console.log(response);
     res.status(201).json(response);
   } else {
-    res.status(500).json('Error in posting review');
+    res.status(500).json('Error in posting note');
   }
 });
