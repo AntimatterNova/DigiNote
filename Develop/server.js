618baecbb54ec7faf399562a165d4a0f0a2554a1
AntimatterNova <dmvsecondary@gmail.com>
2023-06-22 22:29:01 -0500

    finally got the bug figured out. Site works as intended

--- Develop/server.js
+++ Develop/server.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const api = require('./public/assets/js/index.js');
-const uuid = require('./helpers/uuid');
+const { v4: uuidv4 } = require('uuid');
+
 const app = express();
 const PORT = 3001;
+const dbPath = path.join(__dirname, 'db', 'db.json');
 
 app.use(express.static('public'));
 app.use(express.json());
 
 // Loading notes from db.json
 app.get('/api/notes', (req, res) => {
-  const note = JSON.parse(fs.readFileSync(path.join(__dirname, "db", "db.json")));
-  res.json(note);
+  const notes = JSON.parse(fs.readFileSync(dbPath));
+  res.json(notes);
 });
 
 // HTML routes
-app.get('/api/notes', (req, res) => {
+app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
@@ -24,43 +25,58 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
-});
 
 app.post('/api/notes', (req, res) => {
-  // Log that a POST request was received
   console.info(`${req.method} request received to add a note`);
-
-  // Destructuring assignment for the items in req.body
+  
   const { title, text } = req.body;
-
-  // If all the required properties are present
+  
+  
   if (title && text) {
-    // Variable for the object we will save
+   
     const newNote = {
       title,
       text,
-      note_id: uuid(),
+      id: uuidv4(),
     };
-
+    
     const response = {
       status: 'success',
       body: newNote,
     };
-
-    // Read the existing notes from the JSON file
-    const existingNotes = JSON.parse(fs.readFileSync(dbFilePath));
-
-    // Add the new note to the existing notes array
+    
+    
+    const existingNotes = JSON.parse(fs.readFileSync(dbPath));
     existingNotes.push(newNote);
-
-    // Write the updated notes array back to the JSON file
-    fs.writeFileSync(dbFilePath, JSON.stringify(existingNotes));
-
+    
+    
+    fs.writeFileSync(dbPath, JSON.stringify(existingNotes));
     console.log(response);
     res.status(201).json(response);
   } else {
     res.status(500).json('Error in posting note');
   }
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  try {
+    const curNotes = JSON.parse(fs.readFileSync(dbPath));
+    const filtered = curNotes.filter(note => note.id != req.params.id);
+  
+    const response = {
+      status: 'success',
+      body: 'Note deleted',
+    };
+  
+    fs.writeFileSync(dbPath, JSON.stringify(filtered));
+    res.status(201).json(response);
+  }
+  catch(err) {
+    res.status(500).json('Error in deleting the note');
+  }
+  
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
 });
